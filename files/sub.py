18862a1cd7bde76a1e67eb01d8591ef4623786c2
creighton-lewis <creightonlewis01@gmail.com>
2025-10-22 17:59:14 -0500

    Updated sub.py
    -added max time to scan to improve test efficiency
    -added improved error handling for moving different files
    -Commented out subfinder and assetfinder

--- files/sub.py
+++ files/sub.py
@@ -13,11 +13,10 @@ def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 class Sub_Find:
-    def header():
-        console.print(f"""FINDING SUBDOMAINS...""", justify="left", style = "bold green")
     def subdomain_enum():
+        console.print(f"""FINDING SUBDOMAINS...""", justify="left", style = "bold cyan")
         import subprocess
-        target = console.input("\n Write target \n")
+        target = console.input("Write target:")
         sub_list = "wordlists/sub_list"
         file_name1 = f"{target}_dnsbrute"
         subdomains = f"{target}_subdomains"
@@ -25,24 +24,34 @@ class Sub_Find:
         os.system(f"nmap -sV --script dns-brute,dns-service-discovery -sn -n {target} -oN {file_name1}")
         #os.system(f"subfinder -d {target} -o {subdomains}") 
         #os.system(f"assetfinder --subs-only {target} >> {subdomains}")
-        os.system(f"ffuf -w wordlists/sub_list:SUB -u https://SUB.{target} -t 100 -s -of md -o {ffuf_subs} -mc 200-299,302,307 -H "'User-Agent: Mozilla/5.0'"")
-        try:
-                console.print("Extracting urls......")
-                os.system(f"./url_extract.py {ffuf_subs} >> {subdomains}")
-        except: 
-                console.print ("Unable to extract urls")
-        try:
-                console.print("Checking which urls are active....")
-                os.system(f"chmod 700 url_active.sh")
-                os.system(f"./url_active.sh {subdomains}")
-        except:
-                console.print("Unable to check which urls are active")
-        console.print(f"Subdomains found in {subdomains} file.")
-        from results import move_file
-        move_file(subdomains)
-        move_file(ffuf_subs)
-        move_file(file_name1)
-    header()
+        os.system(f"ffuf -w wordlists/sub_list:SUB -u https://SUB.{target} -maxtime 20 -t 100 -s -of md -o {ffuf_subs} -mc 200-299,302,307 -H "'User-Agent: Mozilla/5.0'"")
+        def clean_subs():
+            try:
+                    console.print("Extracting urls......")
+                    os.system(f"~/bin/url_extract.sh {ffuf_subs} >> {subdomains}")
+            except: 
+                    console.print ("Unable to extract urls")
+            try:
+                    console.print("Checking which urls are active....")
+                    os.system(f"chmod 700 url_active.sh")
+                    os.system(f"./url_active.sh {subdomains}")
+            except:
+                    console.print("Unable to check which urls are active")
+            console.print(f"Subdomains found in {subdomains} file.")
+            from results import move_file
+            try:
+                move_file(subdomains)
+            except:
+                console.print("Unable to move subdomains file")
+            try:
+                move_file(file_name1)
+            except:
+                console.print("Unable to move {file_name1} file")
+            try: 
+                move_file(ffuf_subs)
+            except:
+                console.print("Unable to move {ffuf_subs} file")
+        clean_subs()
     subdomain_enum()
     
 

32ab5936cccce4490cb75c4eed586fb5c925cd50
creighton-lewis <creightonlewis01@gmail.com>
2025-10-24 01:05:48 -0500

    Attempted to create and update reverse shell

--- files/shell.py
+++ files/shell.py
@@ -6,11 +6,11 @@ from rich.console import Console #type: ignore
 #Create password 
 #Connect & ssh into server
 #Define target 
-#Determine open ports on target machine 
+#Determine open ports on target machine: use nmap to determine
 #Try to determine target machine type 
 #Record in file 
 #Start simple python server 
-#Upload file 
+#Upload file specific to machine type
 
 console = Console () #type: ignore
 def server_creation():
@@ -22,26 +22,23 @@ def ssh_connect():
     pass_val = sys.argv[2]
     if pass_val == None: 
         pass_val = console.input("Enter password")
+    os.sysetm(f"ssh@{attack_val}")
+    os.system(f"{pass_val}")
 def target_define():
     target_ip = console.input("Enter target ip ")
-    if '.com' in target_ip or '.org' in target_ip or '.net' in target_ip:
+    if '.com' in target_ip or '.org' in target_ip or '.net' in target_ip or '.edu' in target_ip:
         try:
             target_ip = socket.gethostbyname(target_ip)
             console.print(f"Resolved IP: {target_ip}", style="bold green")
         except socket.gaierror:
             console.print("Could not resolve hostname.", style="bold red")
             sys.exit(1)
-    os.system(f"rustscan -a {target_ip}| tee -a open_ports")
+    os.system(f"rustscan -a {target_ip} -- -o {target_ip}_port")
     os.system(f"sed -i 's/{target_ip}//' open_ports") 
-    # this should give a list of ports that look like this
-    80 
-    443 
-    90
-    20
     with open("open_ports", "r") as file: 
         content = file.read() 
         print(content) 
-    for f in open_ports do: 
+    
 #Check what ports are open, prioritizing those under port 1,000
 #Check if url 
 #If url, provide ip address 

230946948a2305b712fa802eec3a6ea4a0a00756
creighton-lewis <creightonlewis01@gmail.com>
2025-10-27 12:13:49 -0500

    Updated install.py for program setup

--- files/install.py
+++ files/install.py
@@ -6,11 +6,15 @@ def bin_scripts():
     loc = os.getcwd
 def check_path():
     if os.path.exists("venv") == False:
-        os.system(f"python3 -m venv venv")
-        os.system(f"source venv/bin/activate")
+        try:
+            os.system(f"python3 -m venv venv")
+            os.system(f"source venv/bin/activate")
+        except:
+           print("Unable to create virtual environment")
     elif os.path.exists("venv") == True:
         os.chdir("venv")
         os.system(f"git clone https://github.com/l4rm4nd/LinkedInDumper.git")
         os.system(f"mv LinkInDumper.git linkedin_info")
         os.system(f"git clone https://github.com/RUB-NDS/PRET.git")
         os.system(f"mv PRET.git pret_tool") 
+        os.system(f"git clone https://raw.githubusercontent.com/praveenkavi5/PathSeeker/refs/heads/main/PathSeeker.py")

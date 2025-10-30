8848feac50cfd8847e3f010baa3d9864a0083b1e
creighton-lewis <creightonlewis01@gmail.com>
2025-10-30 13:51:45 -0500

    removed poc.py
    removed

--- poc.test.py
+++
@@ -1,9 +0,0 @@
-
-
-cve_list = [ 'CVE-2010-1000', 'CVE-2022-1010', 'CVE-2020-0001']
-print (cve_list)
-from files.exploitr.files import Get_POC
-poc = Get_POC()
-
-for cve in cve_list:
-    poc.get_poc(cve_list)

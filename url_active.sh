803db59e0ff2a8b5afaddc1e07319606a2a1f5fd
creighton-lewis <creightonlewis01@gmail.com>
2025-11-10 17:19:00 -0600

    altered colors for url_active.sh

--- url_active.sh
+++ url_active.sh
@@ -4,7 +4,10 @@ file="$1"
 temp_file="temp_active_hosts.txt"
 
 > "$temp_file"
+GREEN='\033[0;32m'
+RED='\033[0;31m'
 
+NC='\033[0m' # No Color
 echo "Checking hosts from '$file'..."
 
 while IFS= read -r host; do
@@ -12,12 +15,13 @@ while IFS= read -r host; do
         continue
     fi
 
-    echo "Pinging $host..."
-    if nmap -sn  "$host" > /dev/null 2>&1; then
-        echo "Host $host is active."
-        echo "$host" >> "$temp_file"
+echo "Pinging $host..."
+if nmap -sn "$host" > /dev/null 2>&1; then
+    echo -e "${GREEN}Host $host is active.${NC}"
+    echo "$host" >> "$temp_file"
+
     else
-        echo "Host $host is inactive or unreachable."
+        echo -e "${RED}Host $host is inactive or unreachable.${NC}"
     fi
 done < "$file"
 

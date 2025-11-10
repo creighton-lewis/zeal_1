5bb15a6de707c4b4b4ae5e770aa72f2389bb1803
creighton-lewis <creightonlewis01@gmail.com>
2025-11-10 17:22:58 -0600

    Update dir.py
    improved text/ messaging

--- files/dir.py
+++ files/dir.py
@@ -15,16 +15,16 @@ def clear_console():
 
 class Dir_Find:
     def dir_enum():
-        console.print("FINDING ADMIN FILES...", style = "bold cyan" , justify = "left")
+        console.print("FINDING Directory FILES...", style = "bold cyan" , justify = "left")
         target = console.input("Write target:")
         dir_list = "wordlists/dir_list"
-        file_name = f"{target}_directories"
-        os.system(f"ffuf -u https://{target}/FUZZ -w {dir_list} -t 100 -s -of md -o {file_name} -p 0.10-0.30 -mc 200-299 -or")
-        os.system(f"./url.sh {file_name}")
+        file_name = "{target}_directories"
+    
+        os.system(f"ffuf -u https://{target}/FUZZ -w {dir_list} -t 100 -of md -o {file_name} -p 0.10-0.30 -mc 200-299")
+        os.system(f"../url_active.sh {file_name}")
         from results import move_file
         move_file(file_name)
     dir_enum()
-
 def main():
     try:
         Dir_Find()

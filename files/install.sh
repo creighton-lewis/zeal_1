99aeb613fdf985ca8a90689c811591b8c00f9b23
creighton-lewis <creightonlewis01@gmail.com>
2025-11-11 15:35:31 -0600

    updated install.sh to have all of the programs that are used in this program

--- files/install.sh
+++ files/install.sh
@@ -3,11 +3,12 @@ echo
 echo "Installing homebrew" 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "Installing nikto" 
-    brew install nikto 
+brew install nikto 
 echo "Installing msfconsole" 
     curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
     chmod 755 msfinstall && \
     ./msfinstall
-echo "Installing rustscan"
 brew install rustscan 
-echo "Installing 
+brew install httpx
+brew install nuclei 
+brew install ffuf 
